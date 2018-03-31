38bc9b58ae77dbbf89ec1a76261a934fd37a6b2f
loi.nguyen <vanloi.elec@gmail.com>
2018-04-01 02:51:13 +0700

    Fix UI same facebook

--- Lokesh/ClientApp/src/app/views/appviews/starterview.component.ts
+++ Lokesh/ClientApp/src/app/views/appviews/starterview.component.ts
@@ -7,19 +7,23 @@ import { Component, OnDestroy, OnInit, } from '@angular/core';
 })
 export class StarterViewComponent implements OnDestroy, OnInit  {
 
-public nav:any;
+  public nav: any;
+  public idwrapper;
 
 public constructor() {
   this.nav = document.querySelector('nav.navbar');
+  this.idwrapper = document.querySelector('#page-wrapper');
 }
 
 public ngOnInit():any {
-  this.nav.className += " white-bg";
+  this.nav.className += " white-bg1";
+  
 }
 
 
 public ngOnDestroy():any {
-  this.nav.classList.remove("white-bg");
+  this.nav.classList.remove("white-bg1");
+  
 }
 
 }
